610da243cf85fd37f0b906bc6f6ddde543edadc9
Tatianemun <160507980+Tatianemun@users.noreply.github.com>
2024-08-27 11:30:38 -0300

    Add files via upload

---
+++ ola.py
@@ -0,0 +1,21 @@
+ola="Olá mundo"
+print(ola)
+
+"""
+criando duas variaveis
+"""
+a=8
+b=12
+somar=a+b
+print("A soma é:",somar)
+
+sub=a-b
+print("A subtração é:",sub)
+
+mult=a*b
+print("A multiplicação é:",mult)
+
+div=a/b
+print("A divisão é:",div)
+
+#
